8b8e9b37317c68f4f2221a6bc1a57dbdc468b013
Martijn <65791391+BoorZuur@users.noreply.github.com>
2026-06-19 16:52:36 +0200

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/signatuuropdracht/',
+  base: '/Kleuren-Code/',
   plugins: [
     react(),
     tailwindcss(),
